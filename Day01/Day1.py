46810edd1b370fa2ce200ed3674e87e71a9a1b51
Paul <pcrodgers1@gmail.com>
2021-12-01 08:20:55 -0700

    Day 1

--- Day01/Day1.py
+++ Day01/Day1.py
@@ -5,3 +5,37 @@ from adventInput import GetInput
 
 _input = GetInput(1)
 
+def part1():
+  counter = 1
+  for i in range(1, len(_input)):
+    prev = _input[i-1]
+    current = _input[i]
+
+    if current > prev:
+      counter += 1
+
+  print(counter)
+
+def part2():
+  counter = 0
+  prevSum = 99999999999999
+  for i in range(len(_input)):
+
+    if i+1 >= len(_input) or i+2 >= len(_input):
+      break
+
+    first = int(_input[i])
+    second = int(_input[(i+1)])
+    third = int(_input[(i+2)])
+
+    if (first + second + third) > prevSum:
+      counter += 1
+    
+    prevSum = first + second + third
+  
+  print(counter)
+
+    
+
+part1()
+part2()
