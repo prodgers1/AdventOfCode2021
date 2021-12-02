c21db781de9ccbd6a57d01b8885a827a5b496ecd
Paul <pcrodgers1@gmail.com>
2021-12-01 22:12:22 -0700

    Day 2 ez

--- Day02/Day2.py
+++ Day02/Day2.py
@@ -4,3 +4,40 @@ sys.path.append('./')
 from adventInput import GetInput
 
 _input = GetInput(2)
+
+
+def part1():
+  x = 0
+  y = 0
+  for line in _input:
+    direction, units = line.split(' ')
+    units = int(units)
+    if direction == 'forward':
+      x += units
+    elif direction == 'down':
+      y += units
+    elif direction == "up":
+      y -= units
+    
+  print(x * y)
+
+def part2():
+  x = 0
+  y = 0
+  aim = 0
+  for line in _input:
+    direction, units = line.split(' ')
+    units = int(units)
+    
+    if direction == 'forward':
+      x += units
+      y += aim * units
+    elif direction == 'down':
+      aim += units
+    elif direction == "up":
+      aim -= units
+    
+  print(x * y)
+
+part1()
+part2()
