a6f6786d46cdfbfcfbc82434e51d735fbff2a8b5
Paul <pcrodgers1@gmail.com>
2021-12-07 16:41:03 -0700

    Day 7

--- Day07/Day7.py
+++ Day07/Day7.py
@@ -1,7 +1,22 @@
 import sys
 import math
 from copy import deepcopy
+from collections import defaultdict
 sys.path.append('./')
 from adventInput import GetInput
 
 _input = GetInput(7)
+_input = [int(x) for x in _input[0].split(',')]
+
+totalGasToMoveTo = defaultdict(int)
+
+for i in range(max(_input)):
+  total = 0
+  for j, val2 in enumerate(_input):    
+    gasToMove = abs(i - val2)
+    total += (gasToMove * (gasToMove + 1)) // 2
+    #total += gasToMove
+  
+  totalGasToMoveTo[i] = total 
+
+print(min([val for x, val in totalGasToMoveTo.items()]))
