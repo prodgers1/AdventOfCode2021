6a1f4b6fed9c666cd28fde1d809ad8ea632c9679
Paul <pcrodgers1@gmail.com>
2022-11-04 15:23:08 -0700

    Does this look better?

--- getDescriptions.py
+++ getDescriptions.py
@@ -12,7 +12,7 @@ for i in range(1,26):
     day = f"0{i}"
   dir_path = os.path.dirname(os.path.realpath(__file__)) + f"\\Day{day}"
 
-  soup = BeautifulSoup(response.text, features="html.parser")
+  soup = BeautifulSoup(response.content, features="html.parser")
   description = soup.find('article', {'class': 'day-desc'}).text
 
   with open(f'{dir_path}\\README.md', 'w') as fp:
